410792d4b21b8f21c8ce59790093057edda8c4d7
Nathan Gawargy <nathan@gawargy.com>
2025-03-25 19:53:50 -0400

    Update setup.py

--- csi_camera_stream/setup.py
+++ csi_camera_stream/setup.py
@@ -1,25 +1,27 @@
-from setuptools import find_packages, setup
+from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'csi_camera_stream'
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version='0.0.1',
+    packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # Explicitly include launch files
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='adminbyte',
-    maintainer_email='nathan@gawargy.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='Your Name',
+    maintainer_email='bytesizedrobotics0@gmail.com',
+    description='CSI Camera Stream Package',
+    license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
-        ],
+        'console_scripts': [],
     },
 )
