ec08ce6b7d20d559671af6641fbcf1e7f4c7b97d
Nathan Gawargy <nathan@gawargy.com>
2025-03-28 10:46:07 -0400

    fix main launch?

--- obstacle_detection/launch/main.launch.py
+++ obstacle_detection/launch/main.launch.py
@@ -2,18 +2,15 @@ import os
 import sys
 
 from ament_index_python.packages import get_package_share_directory
-from launch import LaunchDescription, LaunchIntrospector, LaunchService
-from launch.actions import DeclareLaunchArgument, ExecuteProcess, RegisterEventHandler
-from launch.actions import LogInfo
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
 from launch.conditions import IfCondition
-from launch.event_handlers import OnProcessExit
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-from launch_ros import actions
-
 
 def generate_launch_description():
-    channel_type =  LaunchConfiguration('channel_type', default='serial')
+    # Define launch configurations
+    channel_type = LaunchConfiguration('channel_type', default='serial')
     serial_port = LaunchConfiguration('serial_port', default='/dev/ttyUSB0')
     serial_baudrate = LaunchConfiguration('serial_baudrate', default='115200')
     frame_id = LaunchConfiguration('frame_id', default='laser')
@@ -22,67 +19,60 @@ def generate_launch_description():
     scan_mode = LaunchConfiguration('scan_mode', default='Sensitivity')
     use_rviz = LaunchConfiguration('use_rviz', default='false')
 
+    # Get package share directory
     config_file = os.path.join(get_package_share_directory("nmea_navsat_driver"), "config", "nmea_serial_driver.yaml")
-    
-    rviz_config_dir = os.path.join(
-            get_package_share_directory('rplidar_ros'),
-            'rviz',
-            'rplidar_ros.rviz')
-    
-    
+    rviz_config_dir = os.path.join(get_package_share_directory('rplidar_ros'), 'rviz', 'rplidar_ros.rviz')
+
+    # Define camera node name
+    camera_node_name = 'csi_camera_video'
+
+    # Define nodes
+    camera_node = Node(
+        package='csi_camera_stream',
+        executable=camera_node_name,
+        name=camera_node_name,
+    )
+
+    webrtc_node = Node(
+        package='csi_camera_stream',
+        executable='webRTC_publisher',
+        name='webRTC_publisher',
+    )
+
+    driver_node = Node(
+        package='nmea_navsat_driver',
+        executable='nmea_serial_driver',
+        output='screen',
+        parameters=[config_file]
+    )
+
     return LaunchDescription([
-
-        DeclareLaunchArgument(
-            'channel_type',
-            default_value=channel_type,
-            description='Specifying channel type of lidar'),
-        
-        DeclareLaunchArgument(
-            'serial_port',
-            default_value=serial_port,
-            description='Specifying usb port to connected lidar'),
-
-        DeclareLaunchArgument(
-            'serial_baudrate',
-            default_value=serial_baudrate,
-            description='Specifying usb port baudrate to connected lidar'),
-        
-        DeclareLaunchArgument(
-            'frame_id',
-            default_value=frame_id,
-            description='Specifying frame_id of lidar'),
-
-        DeclareLaunchArgument(
-            'inverted',
-            default_value=inverted,
-            description='Specifying whether or not to invert scan data'),
-
-        DeclareLaunchArgument(
-            'angle_compensate',
-            default_value=angle_compensate,
-            description='Specifying whether or not to enable angle_compensate of scan data'),
-        DeclareLaunchArgument(
-            'scan_mode',
-            default_value=scan_mode,
-            description='Specifying scan mode of lidar'),
-        
-        DeclareLaunchArgument(
-            'use_rviz',
-            default_value='false',
-            description='Flag to start RViz'),
+        # Declare launch arguments
+        DeclareLaunchArgument('channel_type', default_value=channel_type, description='Specifying channel type of lidar'),
+        DeclareLaunchArgument('serial_port', default_value=serial_port, description='Specifying usb port to connected lidar'),
+        DeclareLaunchArgument('serial_baudrate', default_value=serial_baudrate, description='Specifying usb port baudrate to connected lidar'),
+        DeclareLaunchArgument('frame_id', default_value=frame_id, description='Specifying frame_id of lidar'),
+        DeclareLaunchArgument('inverted', default_value=inverted, description='Specifying whether or not to invert scan data'),
+        DeclareLaunchArgument('angle_compensate', default_value=angle_compensate, description='Specifying whether or not to enable angle_compensate of scan data'),
+        DeclareLaunchArgument('scan_mode', default_value=scan_mode, description='Specifying scan mode of lidar'),
+        DeclareLaunchArgument('use_rviz', default_value='false', description='Flag to start RViz'),
 
         # Lidar Driver Node
         Node(
             package='rplidar_ros',
             executable='rplidar_node',
             name='rplidar_node',
-            parameters=[{'channel_type':channel_type,
-                         'serial_port': serial_port,
-                         'serial_baudrate': serial_baudrate,
-                         'frame_id': frame_id,
-                         'inverted': inverted,
-                         'angle_compensate': angle_compensate}],
-            output='screen'),
+            parameters=[{
+                'channel_type': channel_type,
+                'serial_port': serial_port,
+                'serial_baudrate': serial_baudrate,
+                'frame_id': frame_id,
+                'inverted': inverted,
+                'angle_compensate': angle_compensate
+            }],
+            output='screen'
+        ),
+
         # Obstacle Detection Node
         Node(
             package='obstacle_detection',
@@ -97,6 +87,7 @@ def generate_launch_description():
             }],
             output='screen'
         ),
+
         # Rviz
         Node(
             package='rviz2',
@@ -105,41 +96,25 @@ def generate_launch_description():
             arguments=['-d', rviz_config_dir],
             output='screen',
             condition=IfCondition(LaunchConfiguration('use_rviz'))
-            ),
-            
+        ),
+
         # Rosbridge Websocket Server
         Node(
             package='rosbridge_server',
             executable='rosbridge_websocket',
             name='rosbridge_websocket',
             parameters=[{'port': 9090}]
-        )
+        ),
 
-        camera_node_name = 'csi_camera_video'
-
-        # Camera node launch action
-        camera_node = launch_ros.actions.Node(
-            package='csi_camera_stream',
-            executable=camera_node_name,
-            name=camera_node_name,
-        )
-
-        # WebRTC publisher node launch action
-        webrtc_node = launch_ros.actions.Node(
-            package='csi_camera_stream',
-            executable='webRTC_publisher',
-            name='webRTC_publisher',
-        )
-
+        # Motor Control Node
         Node(
             package='wave_rover_serial_control',
             executable='serial_motor_node',
             name='serial_motor_node',
-        )
+        ),
 
-        driver_node = actions.Node(
-            package='nmea_navsat_driver',
-            executable='nmea_serial_driver',
-            output='screen',
-            parameters=[config_file])
+        # Camera Nodes
+        camera_node,
+        webrtc_node,
+        driver_node
     ])
