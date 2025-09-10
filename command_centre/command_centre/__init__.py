bf9b23effc3d72924cdb1b091fdc1c295c2d76e2
Nathan Gawargy <nathan@gawargy.com>
2025-09-09 21:26:12 -0400

    Create __init__.py

---
+++ command_centre/command_centre/__init__.py
@@ -0,0 +1,9 @@
+# Command Centre Package
+"""
+Command Centre and Communication Hub for Autonomous Rover
+
+This package provides the central communication hub for the autonomous rover,
+coordinating all subsystems and handling external commands.
+"""
+
+__version__ = "0.1.0"
