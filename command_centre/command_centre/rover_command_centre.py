9eb8dc6736877559ad783cda0630ee9b2a25d70b
Nathan Gawargy <nathan@gawargy.com>
2025-10-03 11:03:04 -0400

    Update rover_command_centre.py

--- command_centre/command_centre/rover_command_centre.py
+++ command_centre/command_centre/rover_command_centre.py
@@ -262,7 +262,7 @@ class RoverCommandCentre(Node):
         self.rover_state = RoverState.AUTONOMOUS
         
         # Start required nodes for autonomous navigation
-        autonomous_nodes = ['obstacle_detection', 'motor_control', 'gps', 'csi_camera_1', 'imu']
+        autonomous_nodes = ['imu', 'gps', 'obstacle_detection', 'motor_control', 'csi_camera_1']
         
         for node_name in autonomous_nodes:
             if self.node_status[node_name] != NodeStatus.RUNNING:
