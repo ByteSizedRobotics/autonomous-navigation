a17423e9e79c830fcebca4079c32c6454bb22ae6
Nathan Gawargy <nathan@gawargy.com>
2025-10-24 09:57:56 -0400

    testing usb camera

--- command_centre/command_centre/rover_command_centre.py
+++ command_centre/command_centre/rover_command_centre.py
@@ -55,8 +55,8 @@ class RoverCommandCentre(Node):
             'rover': NodeStatus.OFFLINE,
             'csi_camera_1': NodeStatus.OFFLINE,
             'obstacle_detection': NodeStatus.OFFLINE,
-            'manual_control': NodeStatus.OFFLINE
-            #'usb_camera': NodeStatus.OFFLINE
+            'manual_control': NodeStatus.OFFLINE,
+            'usb_camera': NodeStatus.OFFLINE
             # 'motor_control': NodeStatus.OFFLINE
         }        
         # System state
@@ -167,8 +167,8 @@ class RoverCommandCentre(Node):
 		        'rover': 'ros2 run auto_nav rover_serial_bridge',
                 'csi_camera_1': 'ros2 launch csi_camera_stream csi_camera_stream.launch.py',
                 'obstacle_detection': 'ros2 launch obstacle_detection obstacle_detector.launch.py',
-                'manual_control': 'ros2 run potrider wasd_control'
-                #'usb_camera': 'ros2 launch csi_camera_stream usb_camera_stream.launch.py'
+                'manual_control': 'ros2 run potrider wasd_control',
+                'usb_camera': 'ros2 launch csi_camera_stream usb_camera_stream.launch.py'
             }
             
             if node_name in launch_commands:
@@ -279,7 +279,7 @@ class RoverCommandCentre(Node):
             node_kill_targets = {
                 'obstacle_detection': ['rplidar_node', 'obstacle_detector', 'rplidar_composition'],
                 'csi_camera_1': ['csi_camera_inference', 'csi_camera_video', 'csi_camera_snapshot'],
-                #'usb_camera': ['usb_camera_video', 'usb_webRTC_publisher'],
+                'usb_camera': ['usb_camera_video', 'usb_webRTC_publisher'],
                 'gps': ['gps_serial_driver'],
                 'rover': ['rover_serial_bridge'],
                 'manual_control': ['wasd_control']
